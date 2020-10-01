96b77f381085b29f486c35aea65b143f462e5e99
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-10-01 17:06:16 +0100

    better organisation

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,9 @@
 use pyo3::prelude::*;
 use pyo3::PyIterProtocol;
 use pyo3::class::pyasync::PyAsyncProtocol;
-use pyo3::class::pyasync::PyAsyncAwaitProtocol;
-use pyo3::exceptions::{
-    PyStopIteration,
-    PyAssertionError,
-};
 use pyo3::class::iter::IterNextOutput;
-use std::net::{TcpListener, TcpStream, Incoming};
-use std::error::Error;
+use std::net::{TcpListener, TcpStream};
 use std::io;
-use std::borrow::{Borrow, BorrowMut};
-use pyo3::class::iter::IterNextOutput::Yield;
 
 
 ///
@@ -58,14 +50,19 @@ impl AsyncServer {
     }
 }
 
+
+///
+/// The AsyncServerRunner struct houses the TCP listener and sparks the async tasks,
+/// it has a integral clock delay set to n to save cpu todo: find the right match.
+///
 #[pyclass]
 struct AsyncServerRunner {
-    addr: String,
     server: AsyncServer,
     server_state: u8,
     server_exit: bool,
     loop_: PyObject,
     fut: Option<Py<PyAny>>,
+    internal_clock_delay: f32,
 }
 
 #[pymethods]
@@ -74,7 +71,7 @@ impl AsyncServerRunner {
     fn new(binding_addr: String) -> Self {
         println!("Connecting to {}", &binding_addr);
 
-        let server = AsyncServer::new(binding_addr.clone());
+        let server = AsyncServer::new(binding_addr);
         let loop_ = {
             let gil = Python::acquire_gil();
             let py = gil.python();
@@ -82,15 +79,17 @@ impl AsyncServerRunner {
         };
 
         AsyncServerRunner {
-            addr: binding_addr,
             server,
             server_state: 0,
             server_exit: false,
             loop_,
             fut: None,
+            internal_clock_delay: 0.05,
         }
     }
+}
 
+impl AsyncServerRunner {
     fn _sleep(&mut self, py: Python) -> PyResult<()> {
         self.fut = Option::from(self.loop_.call_method0(py, "create_future")?);
 
@@ -98,10 +97,20 @@ impl AsyncServerRunner {
         let _ = self.loop_.call_method1(
             py,
             "call_later",
-            (1, futures.getattr("_set_result_unless_cancelled")?, self.fut.as_ref(), "")
+            (
+                self.internal_clock_delay,
+                futures.getattr("_set_result_unless_cancelled")?,
+                self.fut.as_ref(),
+                "",
+            )
         );
 
-        self.fut = Option::from(self.fut.as_ref().unwrap().call_method0(py, "__iter__")?);
+        self.fut = Option::from(
+            self.fut
+                .as_ref()
+                .unwrap()
+                .call_method0(py, "__iter__")?
+        );
 
         Ok(())
     }
@@ -112,7 +121,7 @@ impl AsyncServerRunner {
 
         // if the future isnt set we'll create a new one
         if self.fut.is_none() {
-            self._sleep(py);
+            let _ = self._sleep(py);
         }
 
         let nxt = self.fut
@@ -122,7 +131,7 @@ impl AsyncServerRunner {
 
         return match nxt {
             Ok(f) => Some(f),
-            Err(er) => {
+            Err(_) => {
                 self.server_state = 1;
                 self.fut = None;
 
@@ -147,21 +156,21 @@ impl PyIterProtocol for AsyncServerRunner {
     fn __next__(mut slf: PyRefMut<Self>) -> PyResult<IterNextOutput<Option<PyObject>, Option<PyObject>>> {
         // setup futures
         if slf.server_state == 0 {
-
             slf.server_state = 1;
         }
 
         // yield futures
         if slf.server_state == 1 {
             let thing = slf.server.accept_client();
-            println!("client {:?}", thing);
-
-
 
             if slf.server_exit {
                 return Ok(IterNextOutput::Return(None))
             }
 
+            if thing.is_some() {
+                println!("client {:?}", thing);
+                return Ok(IterNextOutput::Yield(None))
+            }
             // Lets change our sleep so we sleep for a bit
             slf.server_state = 2;
         }
@@ -174,10 +183,6 @@ impl PyIterProtocol for AsyncServerRunner {
         // Invalid state
         return Ok(IterNextOutput::Return(None))
     }
-
-
-
-
 }
 
 
